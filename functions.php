56f753a3d4a8f2639a7ef7df4b080460764cf3d8
Anne Lamy <138774642+Anne-Lamy@users.noreply.github.com>
2024-02-28 18:20:57 +0100

    Ajout du lien Admin de Motaphoto dans le menu de Wordpress.

--- functions.php
+++ functions.php
@@ -7,4 +7,36 @@ function enqueue_custom_scripts_styles()
 
 }
 
+
+
+
+// On créer notre lien Motaphto dans le menu de Wordpress.
+function motaphoto_add_admin_pages()
+{
+    // Ajout de la page d'administration de Motaphoto.
+    add_menu_page('Paramètre du thème Motaphoto', 'Motaphoto', 'manage_options', 'motaphoto_settings', 'motaphoto_theme_settings', 'dashicons-admin-settings', 60);
+}
+// On créer le contenu de notre page Metaphoto.
+function motaphoto_theme_settings()
+{
+    echo '<h1>' . esc_html(get_admin_page_title()) . '</h1>';
+    echo '<form action="options.php" method="post" name="motaphoto_settings">';
+    echo '<div>';
+
+    settings_fields('motaphoto_settings_fields');
+    do_settings_sections('motaphoto_settings_section');
+    submit_button();
+
+    echo '</div>';
+    echo '</form>';
+}
+
+
+
+
+
+
 add_action('wp_enqueue_scripts', 'enqueue_custom_scripts_styles');
+
+// Appelle le lien Motaphoto dans le menu de Wordpress.
+add_action('admin_menu', 'motaphoto_add_admin_pages', 10);
